6d5e4884290aee5b88a0e35c1a446d7fb892fe1c
Wellington Santana <90020289+Welldevbr@users.noreply.github.com>
2022-11-10 12:00:53 -0300

    Mudanças a serem submetidas: 	modified:   src/components/AlertDialog/index.tsx 	modified:   src/pages/Admin/AdminRoom/index.tsx

--- src/components/AlertDialog/index.tsx
+++ src/components/AlertDialog/index.tsx
@@ -1,16 +1,30 @@
+import { DialogTitle } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
 
-import { useState } from 'react';
+
+import { forwardRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { database } from '../../services/firebase';
 
 type RoomParams = {
   id: string;
 }
+
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
 
 export function AlertDialog() {
   const [open, setOpen] = useState(false);
@@ -57,32 +71,39 @@ export function AlertDialog() {
       <Dialog
         open={open}
         onClose={handleClose}
+        TransitionComponent={Transition}
+        keepMounted
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
+        <DialogTitle textAlign={"center"} style={{fontSize: "1.8rem", marginTop: "20px"}}>
+          {"Você deseja encerrar esta sala?"}
+        </DialogTitle>
         <DialogContent style={{display: "flex", justifyContent: "center"}}>
           <DialogContentText 
-            textAlign={"center"}
+            textAlign={"justify"}
             id="alert-dialog-description"
             style={
               {
-                padding: "20px",
-                margin: "40px",
-                fontSize: "2rem",
+                padding: "5px 10px",
+                margin: "20px 25px",
+                fontSize: "1.6rem",
               }
             }
           >
-              Você realmente deseja <br />
-              encerrar esta sala?
+              Ao encerrar esta sala, os demais <br />
+              usuarios não poderão entrar nesta <br />
+              sala ou até mesmo enviar novas perguntas. <br />
+              você tem certeza que deseja prosseguir?
           </DialogContentText>
         </DialogContent>
         <DialogActions style={{marginInline: "8px"}}>
           <Button 
             style={
               {
-                color: "#e7405f", 
+                color: "#0052a3", 
                 font: "700 1.6rem Roboto",
-                textTransform: "capitalize",
+                textTransform: "lowercase",
               }
             } 
             onClick={handleClose}
@@ -92,15 +113,15 @@ export function AlertDialog() {
           <Button 
             style={
               {
-                color: "#06d6a0", 
+                color: "#e7405f", 
                 font: "700 1.6rem Roboto",
-                textTransform: "capitalize",
+                textTransform: "lowercase",
               }
             }  
             onClick={handleEndRoom} 
             autoFocus
           >
-            Aceitar
+            Encerrar Sala
           </Button>
         </DialogActions>
       </Dialog>
--- src/pages/Admin/AdminRoom/index.tsx
+++ src/pages/Admin/AdminRoom/index.tsx
@@ -17,18 +17,47 @@ import { Question } from '../../../components/Question';
 import { database } from '../../../services/firebase';
 import { AlertDialog } from '../../../components/AlertDialog';
 
+import { DialogTitle } from '@mui/material';
+import Btn from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import { forwardRef, useState } from 'react';
+
 
 type RoomParams = {
   id: string;
 }
 
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
 export function AdminRoom()  {
-  const navigate = useNavigate();
 
   const params = useParams<RoomParams>()
   const roomId: string | any = params.id
 
   const { questions, title } = useRoom( roomId )
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
 
   async function handleCheckeQuestionAsAnswered(questionId: string){
       await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
@@ -48,7 +77,6 @@ export function AdminRoom()  {
       toast.success('Pergunta excluida com sucesso.')
     }
   }
-  //<Button onClick={handleEndRoom} isOutlined>Encerrar Sala</Button>
 
   return (
     <div id="page-room-admin">
@@ -115,13 +143,69 @@ export function AdminRoom()  {
                       <button 
                         type='button' 
                         className='delete-button'
-                        onClick={() => handleDeleteQuestion(question.id)}
+                        onClick={handleClickOpen}
                       >
                         <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M1 5H3H19" stroke="#737380" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                           <path d="M6 5V3C6 2.46957 6.21071 1.96086 6.58579 1.58579C6.96086 1.21071 7.46957 1 8 1H12C12.5304 1 13.0391 1.21071 13.4142 1.58579C13.7893 1.96086 14 2.46957 14 3V5M17 5V19C17 19.5304 16.7893 20.0391 16.4142 20.4142C16.0391 20.7893 15.5304 21 15 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5H17Z" stroke="#737380" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                       </button>
+
+                      <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        TransitionComponent={Transition}
+                        keepMounted
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                      >
+                        <DialogTitle textAlign={"center"} style={{fontSize: "1.8rem", marginTop: "20px"}}>
+                          {"Deletar pergunta"}
+                        </DialogTitle>
+                        <DialogContent style={{display: "flex", justifyContent: "center"}}>
+                          <DialogContentText 
+                            textAlign={"justify"}
+                            id="alert-dialog-description"
+                            style={
+                              {
+                                padding: "2px 10px",
+                                margin: "20px 25px",
+                                fontSize: "1.6rem",
+                              }
+                            }
+                          >
+                             você tem certeza que deseja <br />
+                             deletar esta pergunta da sala?
+                          </DialogContentText>
+                        </DialogContent>
+                        <DialogActions style={{marginInline: "8px"}}>
+                          <Btn 
+                            style={
+                              {
+                                color: "#0052a3", 
+                                font: "700 1.6rem Roboto",
+                                textTransform: "lowercase",
+                              }
+                            } 
+                            onClick={handleClose}
+                          >
+                            cancelar
+                          </Btn>
+                          <Btn 
+                            style={
+                              {
+                                color: "#e7405f", 
+                                font: "700 1.6rem Roboto",
+                                textTransform: "lowercase",
+                              }
+                            }  
+                            onClick={() => handleDeleteQuestion(question.id)} 
+                            autoFocus
+                          >
+                            excluir
+                          </Btn>
+                        </DialogActions>
+                      </Dialog>
                     
                     </Question>
                   </li>
